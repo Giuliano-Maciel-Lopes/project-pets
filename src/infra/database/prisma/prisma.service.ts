3810aec6bbada0f0118d7ca80ecedac7121ba028
Giuliano Maciel <giulianomaciellopes@gmail.com>
2026-01-10 23:10:52 -0300

    feat: conectando o prisma v7 ao nestjs

---
+++ src/infra/database/prisma/prisma.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from 'generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+
+    super({
+      adapter,  // aqui passa o adapter real
+      log: [
+        { level: 'query', emit: 'stdout' },
+        { level: 'warn', emit: 'stdout' },
+        { level: 'error', emit: 'stdout' },
+      ],
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    console.log('✅ Prisma connected');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    console.log('🛑 Prisma disconnected');
+  }
+}
